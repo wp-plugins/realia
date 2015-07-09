d59d68a77f221f7c8da461b8af076c17ec3ff854
pragmaticmates <pragmaticmates@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-09 08:03:18 +0000

    tagging version 0.8.0
    git-svn-id: https://plugins.svn.wordpress.org/realia/trunk@1195269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/misc/breadcrumb.php
+++ templates/misc/breadcrumb.php
@@ -1,5 +1,5 @@
 <ol class="breadcrumb">
-	<?php if ( is_front_page() ): ?>
+	<?php if ( is_front_page() ) :   ?>
 		<li><?php echo __( 'Home', 'realia' ); ?></li>
 		<li><?php bloginfo( 'name' ); ?></li>
 	<?php elseif ( ! is_home() ) : ?>
@@ -9,55 +9,58 @@
         	<?php if ( is_tax() ) : ?>
         		<?php
 	        	global $wp_query;
-        		$tax = $wp_query->get_queried_object();
- 				$term = get_term_by( 'slug', get_query_var( 'term' ), $tax->taxonomy );
-				$ancestors = get_ancestors( $term->term_id, $tax->taxonomy  );
+				$tax = $wp_query->get_queried_object();
+				$term = get_term_by( 'slug', get_query_var( 'term' ), $tax->taxonomy );
+				$ancestors = get_ancestors( $term->term_id, $tax->taxonomy );
 				?>
 
 				<?php if ( is_array( $ancestors ) ) : ?>
 					<?php foreach ( array_reverse( $ancestors ) as $ancestor ) : ?>
 						<li>
 							<?php $term = get_term( $ancestor, $tax->taxonomy ); ?>
-							<a href="<?php echo get_term_link( $term->term_id, $tax->taxonomy); ?>"><?php echo $term->name; ?></a>
+							<a href="<?php echo get_term_link( $term->term_id, $tax->taxonomy ); ?>"><?php echo esc_attr( $term->name ); ?></a>
 						</li>
 					<?php endforeach; ?>
 				<?php endif; ?>
 
 				<li><?php echo single_cat_title(); ?></li>
-        	<?php else: ?>
+        	<?php else : ?>
 	        	<li><?php echo single_cat_title(); ?></li>
 	        <?php endif; ?>
+        <?php elseif ( is_category() ) : ?>
+    		<li>
+                <a href="<?php echo get_post_type_archive_link( get_post_type() ); ?>">
+                    <?php echo get_post_type_object( get_post_type() )->labels->name; ?>
+                </a>
+            </li>
+
+        	<li><?php single_cat_title(); ?></li>	        
         <?php elseif ( is_archive() ) : ?>
         	<li><?php post_type_archive_title(); ?></li>
         <?php endif; ?>
 
         <?php if ( is_category() || is_single() ) : ?>
-        		<li>
-                    <a href="<?php echo get_post_type_archive_link( get_post_type() ); ?>">
-                        <?php echo get_post_type_object( get_post_type() )->labels->name; ?>
-                    </a>
-                </li>
-                <?php if ( is_single() ) : ?>
-					<li><?php the_title(); ?></li>
-                <?php endif; ?>
+            <?php if ( is_single() ) : ?>
+				<li><?php the_title(); ?></li>
+            <?php endif; ?>
         <?php elseif ( is_404() ) : ?>
         	<li><?php echo __( 'Page not found', 'realia' ); ?></li>
-        <?php elseif ( is_page() ): ?>
+        <?php elseif ( is_page() ) :   ?>
                 <li><?php the_title(); ?></li>
         <?php endif; ?>
 	<?php elseif ( is_tag() ) : ?>
 		<li><?php single_tag_title() ?></li>
 	<?php elseif ( is_day() ) : ?>
-		<li><?php echo __( 'Archive for', 'realia'); ?>  <?php the_time( 'F jS, Y' ); ?></li>
-	<?php elseif ( is_month() ): ?>
-		<li><?php echo __( 'Archive for', 'realia'); ?>  <?php the_time( 'F, Y' ); ?></li>
-	<?php elseif (is_year()) : ?>
-		<li><?php echo __( 'Archive for', 'realia'); ?>  <?php the_time( 'Y' ); ?></li>
-	<?php elseif ( is_author() ): ?>
+		<li><?php echo __( 'Archive for', 'realia' ); ?>  <?php the_time( 'F jS, Y' ); ?></li>
+	<?php elseif ( is_month() ) :   ?>
+		<li><?php echo __( 'Archive for', 'realia' ); ?>  <?php the_time( 'F, Y' ); ?></li>
+	<?php elseif ( is_year() ) : ?>
+		<li><?php echo __( 'Archive for', 'realia' ); ?>  <?php the_time( 'Y' ); ?></li>
+	<?php elseif ( is_author() ) :   ?>
 		<li><?php echo __( 'Author Archive', 'realia' ); ?></li>
 	<?php elseif ( isset( $_GET['paged'] ) && ! empty( $_GET['paged'] ) ) : ?>
 		<li><?php echo __( 'Blog Archives', 'realia' ); ?></li>
-	<?php elseif ( is_search()) : ?>
+	<?php elseif ( is_search() ) : ?>
 		<li><?php echo __( 'Search Results', 'realia' ); ?></li>
     <?php endif; ?>
 </ol>
--- templates/misc/register.php
+++ templates/misc/register.php
@@ -1,10 +1,10 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 ?>
 
-<form method="post" action="<?php the_permalink(); ?>">
+<form method="post" action="<?php the_permalink(); ?>" class="register-form">
 	<div class="form-group">
 		<label><?php echo __( 'Username', 'realia' ); ?></label>
 		<input type="text" name="name" class="form-control" required="required">
@@ -26,13 +26,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div><!-- /.form-group -->
 
 
-	<button type="submit" name="register_form"><?php echo __( 'Sign Up', 'realia' ); ?></button>
+	<button type="submit" class="button" name="register_form"><?php echo __( 'Sign Up', 'realia' ); ?></button>
 	<?php $terms = get_theme_mod( 'realia_submission_terms' ); ?>
 
 	<?php if ( ! empty( $terms ) ) : ?>
-		<div class="form-group terms-conditions-input">
+		<div class="checkbox terms-conditions-input">
+			<input type="checkbox" name="agree_terms">
+
 			<label>
-				<input type="checkbox" name="agree_terms">
 				<?php echo sprintf( __( 'I agree with <a href="%s">terms & conditions</a>', 'realia' ), get_permalink( $terms ) ); ?>
 			</label>
 		</div><!-- /.form-group -->
